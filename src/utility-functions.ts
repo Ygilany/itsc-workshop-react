958648574b1d86627094d586b3ad6af4347cd54a
ITSC-conf <itsolctr@ucmail.uc.edu>
2023-12-13 09:49:31 -0500

    mix and match - confusing

--- src/utility-functions.ts
+++ src/utility-functions.ts
@@ -1,4 +1,6 @@
 export const add = (a: number, b: number): number => a + b;
 export const subtract = (a: number, b: number): number => a - b;
-export const multiply = (a: number, b: number): number => a * b;
-export const divide = (a: number, b: number): number => a / b;
+const multiply = (a: number, b: number): number => a * b;
+const divide = (a: number, b: number): number => a / b;
+
+export { multiply, divide };
