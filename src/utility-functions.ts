943b85a598a3fae82ca23a87d7fcc37a98747b54
ITSC-conf <itsolctr@ucmail.uc.edu>
2023-12-13 09:48:57 -0500

    named export 2

--- src/utility-functions.ts
+++ src/utility-functions.ts
@@ -1,6 +1,4 @@
-const add = (a: number, b: number): number => a + b;
-const subtract = (a: number, b: number): number => a - b;
-const multiply = (a: number, b: number): number => a * b;
-const divide = (a: number, b: number): number => a / b;
-
-export { add, subtract, multiply, divide };
+export const add = (a: number, b: number): number => a + b;
+export const subtract = (a: number, b: number): number => a - b;
+export const multiply = (a: number, b: number): number => a * b;
+export const divide = (a: number, b: number): number => a / b;
