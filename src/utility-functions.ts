6275dde187bf31937c5bfd1b51f865b2772590fd
ITSC-conf <itsolctr@ucmail.uc.edu>
2023-12-13 09:52:22 -0500

    utility functions as default export

--- src/utility-functions.ts
+++ src/utility-functions.ts
@@ -1,6 +1,8 @@
-export const add = (a: number, b: number): number => a + b;
-export const subtract = (a: number, b: number): number => a - b;
+const add = (a: number, b: number): number => a + b;
+const subtract = (a: number, b: number): number => a - b;
 const multiply = (a: number, b: number): number => a * b;
 const divide = (a: number, b: number): number => a / b;
 
-export { multiply, divide };
+const utilityFunctions = { add, subtract, multiply, divide };
+
+export default utilityFunctions;
