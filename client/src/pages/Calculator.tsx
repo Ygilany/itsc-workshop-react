34c346d42510656001ab79295b43ab0c5336826a
ITSC-conf <itsolctr@ucmail.uc.edu>
2023-12-14 09:58:05 -0500

    use the utitlity function instead

--- client/src/pages/Calculator.tsx
+++ client/src/pages/Calculator.tsx
@@ -1,48 +1,58 @@
 import { useState } from "react";
+import { add, divide, multiply, subtract } from "../utility-functions";
 
 export const CalculatorPage = () => {
     const [arg1, setArg1] = useState(0);
     const [arg2, setArg2] = useState(0);
+    const [counter, setCounter] = useState(0);
 
-    const [result, setResult] = useState(0);
+    const [result, setResult] = useState<string|number>(0);
     const [operation, setOperation] = useState('?');
 
-    const add = () => {
-        setResult(arg1 + arg2);
+    const onAdd = () => {
+        setResult(add(arg1, arg2));
         setOperation('+');
+        setCounter(counter + 1);
     };
 
-    const subtract = () => {
-        const result = arg1 - arg2;
+    const onSubtract = () => {
+        const result = subtract(arg1, arg2);
         setResult(result);
         setOperation('-');
+        setCounter(counter + 1);
     };
 
-    const multiply = () => {
-        setResult(arg1 * arg2);
+    const onMultiply = () => {
+        setResult(multiply(arg1, arg2));
         setOperation('*');
+        setCounter(counter + 1);
     };
 
-    const divide = () => {
-        setResult(arg1 / arg2);
+    const onDivide = () => {
+        setResult(divide(arg1, arg2));
         setOperation('/');
+        setCounter(counter + 1);
     };
 
     return <>
         <h1>Calculator</h1>
         <div>
             <input type="text" placeholder="Enter first number" onChange={(e) => setArg1(Number(e.target.value))}/>
+
             <input type="text" placeholder="Enter second number" onChange={(e) => setArg2(Number(e.target.value))}/>
+
             <br />
 
-            <button onClick={add}>Add</button>
-            <button onClick={subtract}>Subtract</button>
-            <button onClick={multiply}>Multiply</button>
-            <button onClick={divide}>Divide</button>
+            <button onClick={onAdd}>Add</button>
+            <button onClick={onSubtract}>Subtract</button>
+            <button onClick={onMultiply}>Multiply</button>
+            <button onClick={onDivide}>Divide</button>
 
             <div>
                 <h2>Result: </h2>
                 <h2>{arg1}{operation}{arg2}={result}</h2>
+                <br />
+                <h2>Calculator has been used: {counter}</h2>
             </div>
         </div>
         </>;
--- client/src/utility-functions.ts
+++ client/src/utility-functions.ts
@@ -1,9 +1,11 @@
 const add = (a: number, b: number): number => a + b;
 const subtract = (a: number, b: number): number => a - b;
 const multiply = (a: number, b: number): number => a * b;
-const divide = (a: number, b: number): number => a / b;
+const divide = (a: number, b: number): number | string => {
+    if(b === 0) {
+        return `Cannot divide by 0`;
+    }
+    return a / b;
+};
 
-
-const utilityFunctions = { add, subtract, multiply, divide };
-
-export default utilityFunctions;
+export { add, subtract, multiply, divide };
